8c1cc8fa690f6d48950c3e28c83dbfbb52a7de6d
Arif Onur <onur_eskisehir@hotmail.com>
2025-04-28 11:39:05 +0300

    initial commit

---
+++ NamesGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNameGenerator
+{
+    public class NamesGenerator
+    {
+        private static string[] firstNames = { "James", "Michael", "Robert", "John", "David", "William", "Richard", "Joseph", "Thomas", "Charles" };
+        private static string[] lastNames = { "Smith", "Brown", "Johnson", "Jones", "Williams", "Thomas", "Miller", "Davis", "Taylor", "Jackson", "Martin" };
+
+        public static string GenerateName()
+        {
+            var rand = new Random();
+            string randomFirstName = firstNames[rand.Next(firstNames.Length)];
+            string randomLastName = lastNames[rand.Next(lastNames.Length)];
+
+            return $"{randomFirstName} {randomLastName}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using RandomNameGenerator;
+
+namespace RandomNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomName = NamesGenerator.GenerateName();
+            Console.WriteLine(randomName);
+
+            Console.ReadLine();
+        }
+    }
+}
